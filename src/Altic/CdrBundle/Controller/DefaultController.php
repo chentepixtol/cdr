30264d69164b00595aa8554d6e84576284a049cd
Vicente Mendoza <vicentemmor@yahoo.com.mx>
2012-06-02 21:14:47 -0500

    map reduce

--- src/Altic/CdrBundle/Controller/DefaultController.php
+++ src/Altic/CdrBundle/Controller/DefaultController.php
@@ -6,11 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+    /**
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function indexAction()
     {
         $request = $this->getRequest();
         $records = array();
-        if( $request->isMethod("POST")){
+        if( $request->isMethod("POST") ){
             $filter = array();
             $startDate = $request->request->get('start_date');
             $endDate = $request->request->get('end_date');
@@ -29,6 +33,48 @@ class DefaultController extends Controller
         }
         return $this->render('AlticCdrBundle:Default:index.html.twig', array('records' => $records, 'params' => $request->request->all()));
     }
+
+    /**
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function singleSeriesChartAction(){
+
+        $params = $this->getRequest()->request;
+
+        if( $this->getRequest()->isMethod("POST") ){
+            $field = $params->get('field');
+
+            $result = $this->getMongoCdr()->getCountByField($field);
+            echo "<pre>";print_r($result);die();
+
+        }
+
+
+        return $this->render('AlticCdrBundle:Default:single.html.twig', array());
+    }
+
+    /**
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function multipleSeriesChartAction(){
+
+        $params = $this->getRequest()->request;
+
+        if( $this->getRequest()->isMethod("POST") ){
+            $field = $params->get('field');
+            $fieldTwo = $params->get('fieldTwo');
+
+            $result = $this->getMongoCdr()->getCountByFields($field, $fieldTwo);
+            echo "<pre>";print_r($result);die();
+
+        }
+
+        return $this->render('AlticCdrBundle:Default:multiple.html.twig', array());
+    }
+
+
 
     /**
      * @return \Altic\CdrBundle\Mongo\Cdr
--- src/Altic/CdrBundle/Mongo/Cdr.php
+++ src/Altic/CdrBundle/Mongo/Cdr.php
@@ -21,4 +21,76 @@ class Cdr
         return $this->collection;
     }
 
+    /**
+     *
+     * @param string $field
+     * @throws \Exception
+     */
+    public function getCountByField($field){
+        $cursor = $this->mapReduce("cdr", "function(){
+            emit(this.{$field}, {cont: 1});
+        }", "function(key, values){
+                var obj = {cont: 0}
+                for(i in values){
+                    obj.cont += values[i].cont;
+                }
+                return obj;
+        }", "groupByField");
+
+        $result = array();
+        foreach ($cursor as $doc){
+            $result[$doc['_id']] = $doc['value']['cont'] ?: 0;
+        }
+
+        return $result;
+    }
+
+    /**
+     *
+     * @param string $field
+     * @throws \Exception
+     */
+    public function getCountByFields($fieldOne, $fieldTwo){
+        $cursor = $this->mapReduce("cdr", "function(){
+            var obj = {key1: this.{$fieldOne}, key2: this.{$fieldTwo}};
+            emit(obj, {cont: 1});
+        }", "function(key, values){
+            var obj = {cont: 0}
+            for(i in values){
+                obj.cont += values[i].cont;
+            }
+            return obj;
+        }", "groupByFields");
+
+        $result = array();
+        foreach ($cursor as $doc){
+            $result[$doc['_id']['key1']][$doc['_id']['key2']] = $doc['value']['cont'];
+        }
+
+        return $result;
+    }
+
+    /**
+     *
+     * @param string $collection
+     * @param string $mapFunction
+     * @param string $reduceFunction
+     * @param string $outCollection
+     * @return array
+     * @throws \Exception
+     */
+    private function mapReduce($collection, $mapFunction, $reduceFunction, $outCollection){
+        $result = $this->db->getDB()->command(array(
+            "mapreduce" => $collection,
+            "map" => new \MongoCode($mapFunction),
+            "reduce" => new \MongoCode($reduceFunction),
+            "out" => array("replace" => $outCollection))
+        );
+
+        if( isset($result['ok']) && $result['ok'] ){
+            return $this->db->getDB()->selectCollection($result['result'])->find();
+        }else {
+            throw new \Exception(json_encode($result));
+        }
+    }
 }
